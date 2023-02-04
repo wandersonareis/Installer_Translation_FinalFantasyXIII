83f69bb89d9765b4bb57046ef201a13a5b35f0b8
WandersonAReis <wandersonfrost8@hotmail.com>
2023-02-04 15:58:57 -0300

    Adicionado serviço de localização

---
+++ src/Installer.Common/localization/Localization.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using libc.translation;
+
+namespace Installer.Common.localization;
+
+public class Localization
+{
+    private static readonly Assembly Assembly = Assembly.GetExecutingAssembly();
+    private const string ResourceId = "Installer.Common.localization.localizations.json";
+    private static readonly ILocalizationSource Source = new JsonLocalizationSource(Assembly, ResourceId, PropertyCaseSensitivity.CaseSensitive);
+    public static readonly ILocalizer Localizer = new Localizer(Source, "en");
+}
