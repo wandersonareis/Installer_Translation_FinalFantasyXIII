ee9fdc4ca4da7135cd65814160ae5b15507d5cae
WandersonAReis <wandersonfrost8@hotmail.com>
2023-02-22 00:48:55 -0300

    colocado ano limite no timestamp

--- src/Installer.Common/Framework/Extensions/ConvertersExt.cs
+++ src/Installer.Common/Framework/Extensions/ConvertersExt.cs
@@ -4,10 +4,11 @@ namespace Installer.Common.Framework.Extensions;
 
 public static class ConvertersExt
 {
-    public static string ConvertFromUnixTimestamp(this string timestamp, string format = "dd-MM-yyyy")
+    public static string ConvertFromUnixTimestamp(this string stamp, string format = "dd-MM-yyyy")
     {
-        return timestamp == "0" ? 
+        long timestamp = long.Parse(stamp);
+        return timestamp > 1641006000 ?
             Localization.Localizer.Get("Warning.WithoutInstalledVersion") :
-            DateTimeOffset.FromUnixTimeSeconds(long.Parse(timestamp)).ToLocalTime().ToString(format);
+            DateTimeOffset.FromUnixTimeSeconds(timestamp).ToLocalTime().ToString(format);
     }
 }
