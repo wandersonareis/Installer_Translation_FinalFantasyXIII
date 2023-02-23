20f397e6fedb77155b369b6946a0972a2f57702e
WandersonAReis <wandersonfrost8@hotmail.com>
2023-02-23 15:28:38 -0300

    Mudanças método de conversão unix para data
    Adicionado tipo string e long em ConvertFromUnixTimestamp

--- src/Installer.Common/Framework/Extensions/ConvertersExt.cs
+++ src/Installer.Common/Framework/Extensions/ConvertersExt.cs
@@ -4,11 +4,11 @@ namespace Installer.Common.Framework.Extensions;
 
 public static class ConvertersExt
 {
-    public static string ConvertFromUnixTimestamp(this string stamp, string format = "dd-MM-yyyy")
-    {
-        long timestamp = long.Parse(stamp);
-        return timestamp > 1641006000 ?
+    public static string ConvertFromUnixTimestamp(this string timestamp, string format = "dd-MM-yyyy") =>
+        long.Parse(timestamp).ConvertFromUnixTimestamp(format);
+
+    public static string ConvertFromUnixTimestamp(this long timestamp, string format = "dd-MM-yyyy") =>
+        timestamp < 1641006000 ?
             Localization.Localizer.Get("Warning.WithoutInstalledVersion") :
             DateTimeOffset.FromUnixTimeSeconds(timestamp).ToLocalTime().ToString(format);
-    }
 }
--- src/Installer.Common/IPersistenceRegisterProvider.cs
+++ src/Installer.Common/IPersistenceRegisterProvider.cs
@@ -4,12 +4,9 @@ public interface IPersistenceRegisterProvider
 {
     string GetGamePath();
 
-    string GetInstalledTranslation();
+    long GetInstalledTranslation();
 
     void SetGamePath(string path);
 
     void SetInstalledTranslation(string id);
-
-    string GetDateFromInstalledTranslation();
-
 }
